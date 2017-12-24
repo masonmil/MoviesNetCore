1decc78ed73d6388c8375a8d168e3d34c41d62a5
masonmil <masonis@me.com>
2017-12-24 11:25:44 -0500

    testing cs file

--- MoviesSln/Movies/Startup.cs
+++ MoviesSln/Movies/Startup.cs
@@ -27,7 +27,7 @@ namespace Movies
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Mason World!");
             });
         }
     }
